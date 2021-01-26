7b2184c91b5a1eebad6ce2036fa55a8429f41cf0
BartassikF <78033227+BartassikF@users.noreply.github.com>
2021-01-26 16:26:23 +0100

    Sortowanie babelkowe

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void sortowanie_babelkowe(int tab[],int n)
+{
+	for(int i=0;i<n;i++)
+		for(int j=1;j<n-i;j++) //pêtla wewnêtrzna
+		if(tab[j-1]>tab[j])
+			//zamiana miejscami
+			swap(tab[j-1], tab[j]);
+}
+
+int main()
+{
+	int *tab, n;
+
+	cout<<"Ile liczb bêdziesz chcia³ posortowaæ? ";
+	cin>>n;
+
+	tab = new int [n]; //przydzielenie pamiêci na elementy tablicy
+	//wczytanie liczb
+	for(int i=0;i<n;i++)
+		cin>>tab[i];
+
+	sortowanie_babelkowe(tab,n);
+
+	//wypisanie posortowanych elementów
+	for(int i=0;i<n;i++)
+          cout<<tab[i]<<" ";
+
+  return 0;
+}
